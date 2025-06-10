e3a9480a299be01ad97aba5aeb3ea239a6791d22
HENU-er <zxc820424@gmail.com>
2025-06-10 20:25:11 +0800

    2025-6-10

---
+++ build-Test001-Desktop_Qt_5_12_9_MinGW_64_bit-Release/release/moc_widget.cpp
@@ -0,0 +1,246 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.9)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Test001/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.9. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[31];
+    char stringdata0[622];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 19), // "StartBackgroundMove"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 12), // "StartCarMove"
+QT_MOC_LITERAL(4, 41, 14), // "StartPlaneMove"
+QT_MOC_LITERAL(5, 56, 22), // "StartGameButtonClicked"
+QT_MOC_LITERAL(6, 79, 11), // "ExitClicked"
+QT_MOC_LITERAL(7, 91, 11), // "BackClicked"
+QT_MOC_LITERAL(8, 103, 17), // "GameOptionClicked"
+QT_MOC_LITERAL(9, 121, 18), // "ClassicGameClicked"
+QT_MOC_LITERAL(10, 140, 25), // "ClassicGameBackgroundMove"
+QT_MOC_LITERAL(11, 166, 24), // "ClassicGamePlane_CarMove"
+QT_MOC_LITERAL(12, 191, 21), // "ClassicGamePlayerMove"
+QT_MOC_LITERAL(13, 213, 35), // "ClassicGamePlane_CarMoveSpeed..."
+QT_MOC_LITERAL(14, 249, 28), // "ClassicGamePlayerHealthCheck"
+QT_MOC_LITERAL(15, 278, 18), // "ClassicGameStepTwo"
+QT_MOC_LITERAL(16, 297, 30), // "ClassicGameReloadButtonClicked"
+QT_MOC_LITERAL(17, 328, 27), // "ClassicGameEndButtonClicked"
+QT_MOC_LITERAL(18, 356, 22), // "ClassicGameScoreUpdate"
+QT_MOC_LITERAL(19, 379, 24), // "ClassicGameStep3AddPlane"
+QT_MOC_LITERAL(20, 404, 20), // "ClassicGameStepThree"
+QT_MOC_LITERAL(21, 425, 34), // "ClassicGameStepTwoPlayerTypeC..."
+QT_MOC_LITERAL(22, 460, 15), // "StepTowBossMove"
+QT_MOC_LITERAL(23, 476, 17), // "StepThreeBossMove"
+QT_MOC_LITERAL(24, 494, 15), // "StepTwoBossDead"
+QT_MOC_LITERAL(25, 510, 17), // "StepThreeBossDead"
+QT_MOC_LITERAL(26, 528, 11), // "BulletShoot"
+QT_MOC_LITERAL(27, 540, 21), // "ClassicGamePlane1Dead"
+QT_MOC_LITERAL(28, 562, 21), // "ClassicGamePlane2Dead"
+QT_MOC_LITERAL(29, 584, 18), // "ClassicGameCarDead"
+QT_MOC_LITERAL(30, 603, 18) // "MultiplayerClicked"
+
+    },
+    "Widget\0StartBackgroundMove\0\0StartCarMove\0"
+    "StartPlaneMove\0StartGameButtonClicked\0"
+    "ExitClicked\0BackClicked\0GameOptionClicked\0"
+    "ClassicGameClicked\0ClassicGameBackgroundMove\0"
+    "ClassicGamePlane_CarMove\0ClassicGamePlayerMove\0"
+    "ClassicGamePlane_CarMoveSpeedChange\0"
+    "ClassicGamePlayerHealthCheck\0"
+    "ClassicGameStepTwo\0ClassicGameReloadButtonClicked\0"
+    "ClassicGameEndButtonClicked\0"
+    "ClassicGameScoreUpdate\0ClassicGameStep3AddPlane\0"
+    "ClassicGameStepThree\0"
+    "ClassicGameStepTwoPlayerTypeChange\0"
+    "StepTowBossMove\0StepThreeBossMove\0"
+    "StepTwoBossDead\0StepThreeBossDead\0"
+    "BulletShoot\0ClassicGamePlane1Dead\0"
+    "ClassicGamePlane2Dead\0ClassicGameCarDead\0"
+    "MultiplayerClicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  159,    2, 0x0a /* Public */,
+       3,    0,  160,    2, 0x0a /* Public */,
+       4,    0,  161,    2, 0x0a /* Public */,
+       5,    0,  162,    2, 0x0a /* Public */,
+       6,    0,  163,    2, 0x0a /* Public */,
+       7,    0,  164,    2, 0x0a /* Public */,
+       8,    0,  165,    2, 0x0a /* Public */,
+       9,    0,  166,    2, 0x0a /* Public */,
+      10,    0,  167,    2, 0x0a /* Public */,
+      11,    0,  168,    2, 0x0a /* Public */,
+      12,    0,  169,    2, 0x0a /* Public */,
+      13,    0,  170,    2, 0x0a /* Public */,
+      14,    0,  171,    2, 0x0a /* Public */,
+      15,    0,  172,    2, 0x0a /* Public */,
+      16,    0,  173,    2, 0x0a /* Public */,
+      17,    0,  174,    2, 0x0a /* Public */,
+      18,    0,  175,    2, 0x0a /* Public */,
+      19,    0,  176,    2, 0x0a /* Public */,
+      20,    0,  177,    2, 0x0a /* Public */,
+      21,    0,  178,    2, 0x0a /* Public */,
+      22,    0,  179,    2, 0x0a /* Public */,
+      23,    0,  180,    2, 0x0a /* Public */,
+      24,    0,  181,    2, 0x0a /* Public */,
+      25,    0,  182,    2, 0x0a /* Public */,
+      26,    0,  183,    2, 0x0a /* Public */,
+      27,    0,  184,    2, 0x0a /* Public */,
+      28,    0,  185,    2, 0x0a /* Public */,
+      29,    0,  186,    2, 0x0a /* Public */,
+      30,    0,  187,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Widget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->StartBackgroundMove(); break;
+        case 1: _t->StartCarMove(); break;
+        case 2: _t->StartPlaneMove(); break;
+        case 3: _t->StartGameButtonClicked(); break;
+        case 4: _t->ExitClicked(); break;
+        case 5: _t->BackClicked(); break;
+        case 6: _t->GameOptionClicked(); break;
+        case 7: _t->ClassicGameClicked(); break;
+        case 8: _t->ClassicGameBackgroundMove(); break;
+        case 9: _t->ClassicGamePlane_CarMove(); break;
+        case 10: _t->ClassicGamePlayerMove(); break;
+        case 11: _t->ClassicGamePlane_CarMoveSpeedChange(); break;
+        case 12: _t->ClassicGamePlayerHealthCheck(); break;
+        case 13: _t->ClassicGameStepTwo(); break;
+        case 14: _t->ClassicGameReloadButtonClicked(); break;
+        case 15: _t->ClassicGameEndButtonClicked(); break;
+        case 16: _t->ClassicGameScoreUpdate(); break;
+        case 17: _t->ClassicGameStep3AddPlane(); break;
+        case 18: _t->ClassicGameStepThree(); break;
+        case 19: _t->ClassicGameStepTwoPlayerTypeChange(); break;
+        case 20: _t->StepTowBossMove(); break;
+        case 21: _t->StepThreeBossMove(); break;
+        case 22: _t->StepTwoBossDead(); break;
+        case 23: _t->StepThreeBossDead(); break;
+        case 24: _t->BulletShoot(); break;
+        case 25: _t->ClassicGamePlane1Dead(); break;
+        case 26: _t->ClassicGamePlane2Dead(); break;
+        case 27: _t->ClassicGameCarDead(); break;
+        case 28: _t->MultiplayerClicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Widget::staticMetaObject = { {
+    &QWidget::staticMetaObject,
+    qt_meta_stringdata_Widget.data,
+    qt_meta_data_Widget,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 29;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
